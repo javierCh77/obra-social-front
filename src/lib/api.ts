bbe101f244b3c70fc9fc59ed022994cb1f543a49
javierCh77 <javierchavarria21@gmail.com>
2025-05-23 03:30:18 -0300

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3006/api',
+  baseURL: 'http://localhost:3006/api',
   withCredentials: true,
 });
 
